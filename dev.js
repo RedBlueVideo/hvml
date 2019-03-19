fb8be95a34fa3d669eb42c96856400bf0b41d601
Hugh Guiney <hugh@hughguiney.com>
2019-03-19 03:27:45 -0400

    wip → master

--- dev.js
+++
@@ -1,7 +0,0 @@
-const HVML = require( './index.js' );
-
-const hvml = new HVML( './examples/hvml.xml' );
-hvml.ready.then( () => {
-  // Instance methods available
-  console.log( hvml.validate() );
-} );
