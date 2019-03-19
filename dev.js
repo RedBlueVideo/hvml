844d9fb61e99320281f1998a3f529490a40f27ce
Hugh Guiney <hugh@hughguiney.com>
2019-03-19 03:23:48 -0400

    dev.js

--- dev.js
+++ dev.js
@@ -1,4 +1,7 @@
-const fs = require( 'fs' );
-const xml = require( 'libxmljs' );
-const { exec } = require( 'child_process' );
 const HVML = require( './index.js' );
+
+const hvml = new HVML( './examples/hvml.xml' );
+hvml.ready.then( () => {
+  // Instance methods available
+  console.log( hvml.validate() );
+} );
