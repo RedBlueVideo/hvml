2fcc4df12326294d319ce79a85c548ac25b7a49e
Hugh Guiney <hugh@hughguiney.com>
2019-04-01 04:52:45 -0400

    errors.js → validation.js

---
+++ validation.js
@@ -0,0 +1,93 @@
+// https://rclayton.silvrback.com/custom-errors-in-node-js
+class HVMLDomainError extends Error {
+  constructor( message ) {
+    super( message );
+    // Ensure the name of this error is the same as the class name
+    this.name = this.constructor.name;
+    // This clips the constructor invocation from the stack trace.
+    // It's not absolutely essential, but it does make the stack trace a little nicer.
+    //  @see Node.js reference (bottom)
+    Error.captureStackTrace( this, this.constructor );
+  }
+}
+
+class HVMLEnumError extends HVMLDomainError {
+  /* className, field, badValues */
+  constructor( data ) {
+    super( `The following values are invalid for ${data.className}::${data.field}: ${data.badValues.join( ', ' )}` );
+    this.data = data;
+  }
+}
+
+class HVMLTypeError extends HVMLDomainError {
+  /* className, field, expected, extraInfo = '' */
+  constructor( data ) {
+    if ( !data.extraInfo ) {
+      data.extraInfo = null;
+    }
+
+    if ( !data.got && data.input ) {
+      data.got = data.input.constructor.name;
+    }
+
+    super( `${data.className}::${data.field} must be of type ${data.expected}; got ${data.got}${data.extraInfo || ''}` );
+    this.data = data;
+  }
+}
+
+class HVMLRangeError extends HVMLTypeError {
+  /* className, field, expected, lowerBound, upperBound, exclusivity = 'inclusive' */
+  constructor( data ) {
+    let lowerBoundDefined;
+    let upperBoundDefined;
+    let extraInfo;
+
+    if ( typeof data.lowerBound !== 'undefined' ) {
+      lowerBoundDefined = true;
+    } else {
+      data.lowerBound = Number.NEGATIVE_INFINITY;
+    }
+
+    if ( typeof data.upperBound !== 'undefined' ) {
+      upperBoundDefined = true;
+    } else {
+      data.upperBound = Number.POSITIVE_INFINITY;
+    }
+
+    if ( !data.exclusivity ) {
+      data.exclusivity = 'inclusive';
+    }
+
+    if ( lowerBoundDefined && !upperBoundDefined ) {
+      extraInfo = ` with a value greater than or equal to ${data.lowerBound}`;
+    } else if ( !lowerBoundDefined && upperBoundDefined ) {
+      extraInfo = ` with a value less than or equal to ${data.upperBound}`;
+    } else if ( lowerBoundDefined && upperBoundDefined ) {
+      extraInfo = ` with a value between ${data.lowerBound} and ${data.upperBound} (${data.exclusivity})`;
+    } else {
+      extraInfo = ``;
+    }
+
+    super( {
+      ...data,
+      extraInfo,
+    } );
+    this.data = data;
+  }
+}
+
+class HVMLNotIntegerError extends HVMLTypeError {
+  constructor( data ) {
+    super( {
+      ...data,
+      "expected": "Integer",
+    } );
+  }
+}
+
+module.exports = {
+  "EnumError": HVMLEnumError,
+  "TypeError": HVMLTypeError,
+  "RangeError": HVMLRangeError,
+  "NotIntegerError": HVMLNotIntegerError,
+};
