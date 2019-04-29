3bd6c2671eff8d2a2039565740aaa8e3ef119cbd
Hugh Guiney <hugh@hughguiney.com>
2019-04-28 20:45:51 -0400

    npmify (one-off solution) → npm-minify (dependency)

---
+++ .npmminify.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "filter": [
+    "**/*.js",
+    "rng/hvml.rng",
+    "!**/*.test.js",
+    "!jest.config.js",
+    "!node_modules/**",
+    "!coverage/**",
+    "!dev.js",
+    "!npmify.js",
+  ]
+};
